4862668083296cb2c488e855e759cf6d08a63ed8
anh-trinh <anh-trinh@cybozu.vn>
2019-05-06 17:00:06 +0700

    add lint command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import * as program from 'commander';
+import * as program from 'commander'
 import docCommand from './commands/doc/docCommand'
 import configCommand from './commands/config/configCommand'
-import dataCommand from './commands/data/dataCommand';
+import dataCommand from './commands/data/dataCommand'
 import initializeCommand from './commands/Initialize/initializeCommand'
+import lintCommand from './commands/Lint/lintCommand'
 import * as path from 'path';
 global['cliRoot'] = path.resolve(path.dirname(require.main.filename) + "/../");
 global['currentDir'] = process.cwd();
@@ -16,5 +17,6 @@ docCommand(program)
 configCommand(program)
 dataCommand(program)
 initializeCommand(program)
+lintCommand(program)
 
 program.parse(process.argv);
