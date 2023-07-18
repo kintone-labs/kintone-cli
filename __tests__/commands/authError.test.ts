dbdecb16f24767d1997362ef005b0b0bcc7cbda3
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-07-18 10:45:03 +0700

    [chore]: add await

--- __tests__/commands/authError.test.ts
+++ __tests__/commands/authError.test.ts
@@ -31,70 +31,96 @@ const ORIGINAL_CWD = process.cwd();
 const TEMP_DIR = ORIGINAL_CWD + '/__tests__/authErrorTemp';
 
 describe('auth command: errors', () => {
+  let mainProgram: CommanderStatic;
+
+  beforeAll(async () => {
+    createTempDir(TEMP_DIR);
+
+    await initProject(TEMP_DIR, PROJECT_NAME);
+    await createTemplate(TEMP_DIR, PROJECT_NAME);
+  });
+
+  afterAll(() => {
+    removeTempDir(TEMP_DIR);
+  });
+
+  test('should throw error "App name missing" when no app name is specified', async () => {
+    const consoleLogSpy = jest.spyOn(global.console, 'log');
+    mainProgram = authCommand(program);
+    process.argv = ['node', 'auth'];
+    await mainProgram.parseAsync(process.argv);
+
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      chalk.red(ERRORS.APP_NAME_MISSING)
+    );
+  });
+
   test(`The name app missing -> ${ERRORS.APP_NAME_MISSING}`, async () => {
     const input = '';
-    expect(authValidator(input)).resolves.toBe(ERRORS.APP_NAME_MISSING);
+    await expect(authValidator(input)).resolves.toBe(ERRORS.APP_NAME_MISSING);
   });
 
   test(`Domain validator: missing https -> ${ERRORS.DOMAIN_STARTS_WITH_HTTPS}`, async () => {
     const input = 'domain.kintone.com';
-    expect(domainValidator(input)).resolves.toBe(
+    await expect(domainValidator(input)).resolves.toBe(
       ERRORS.DOMAIN_STARTS_WITH_HTTPS
     );
   });
 
   test(`Domain validator: valid domain -> ${ERRORS.VALID_DOMAIN}`, async () => {
     const input = 'https://%*@(&#$#).k#intone.%com';
-    expect(domainValidator(input)).resolves.toBe(ERRORS.VALID_DOMAIN);
+    await expect(domainValidator(input)).resolves.toBe(ERRORS.VALID_DOMAIN);
   });
 
   test(`Domain validator: true -> ${ERRORS.VALID_DOMAIN}`, async () => {
     const input = 'https://domain.kintone.com';
-    expect(domainValidator(input)).resolves.toBe(true);
+    await expect(domainValidator(input)).resolves.toBe(true);
   });
 
   test(`Username validator -> ${ERRORS.USER_NAME_EMPTY}`, async () => {
     const input = '';
-    expect(usernameValidator(input)).resolves.toBe(ERRORS.USER_NAME_EMPTY);
+    await expect(usernameValidator(input)).resolves.toBe(
+      ERRORS.USER_NAME_EMPTY
+    );
   });
 
   test(`Username validator -> true`, async () => {
     const input = 'tester';
-    expect(usernameValidator(input)).resolves.toBe(true);
+    await expect(usernameValidator(input)).resolves.toBe(true);
   });
 
   test(`Password validator -> ${ERRORS.PASSWORD_EMPTY}`, async () => {
     const input = '';
-    expect(passwordValidator(input)).resolves.toBe(ERRORS.PASSWORD_EMPTY);
+    await expect(passwordValidator(input)).resolves.toBe(ERRORS.PASSWORD_EMPTY);
   });
 
   test(`Password validator -> true`, async () => {
     const input = 'passwordtest';
-    expect(passwordValidator(input)).resolves.toBe(true);
+    await expect(passwordValidator(input)).resolves.toBe(true);
   });
 
   test(`AppID validator -> ${ERRORS.APP_ID_EMPTY}`, async () => {
     const input = '';
-    expect(appIDValidator(input)).resolves.toBe(ERRORS.APP_ID_EMPTY);
+    await expect(appIDValidator(input)).resolves.toBe(ERRORS.APP_ID_EMPTY);
   });
 
   test(`AppID validator -> ${ERRORS.APP_ID_NUMBER}`, async () => {
     const input = '%#$testttt';
-    expect(appIDValidator(input)).resolves.toBe(ERRORS.APP_ID_NUMBER);
+    await expect(appIDValidator(input)).resolves.toBe(ERRORS.APP_ID_NUMBER);
   });
 
   test(`AppID validator -> true`, async () => {
     const input = '546';
-    expect(appIDValidator(input)).resolves.toBe(true);
+    await expect(appIDValidator(input)).resolves.toBe(true);
   });
 
   test(`Proxy validator -> ${ERRORS.PROXY_EMPTY}`, async () => {
     const input = '';
-    expect(proxyValidator(input)).resolves.toBe(ERRORS.PROXY_EMPTY);
+    await expect(proxyValidator(input)).resolves.toBe(ERRORS.PROXY_EMPTY);
   });
 
   test(`Proxy validator -> true`, async () => {
     const input = '546';
-    expect(proxyValidator(input)).resolves.toBe(true);
+    await expect(proxyValidator(input)).resolves.toBe(true);
   });
 });
