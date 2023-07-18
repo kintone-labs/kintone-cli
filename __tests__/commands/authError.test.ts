b997b4156e175fcd44c5b653ddadfa7f3b4b7bc7
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-07-18 10:39:52 +0700

    [chore]: remove unuse

--- __tests__/commands/authError.test.ts
+++ __tests__/commands/authError.test.ts
@@ -31,30 +31,6 @@ const ORIGINAL_CWD = process.cwd();
 const TEMP_DIR = ORIGINAL_CWD + '/__tests__/authErrorTemp';
 
 describe('auth command: errors', () => {
-  let mainProgram: CommanderStatic;
-
-  beforeAll(async () => {
-    createTempDir(TEMP_DIR);
-
-    await initProject(TEMP_DIR, PROJECT_NAME);
-    await createTemplate(TEMP_DIR, PROJECT_NAME);
-  });
-
-  afterAll(() => {
-    removeTempDir(TEMP_DIR);
-  });
-
-  test('should throw error "App name missing" when no app name is specified', async () => {
-    const consoleLogSpy = jest.spyOn(global.console, 'log');
-    mainProgram = authCommand(program);
-    process.argv = ['node', 'auth'];
-    await mainProgram.parseAsync(process.argv);
-
-    expect(consoleLogSpy).toHaveBeenCalledWith(
-      chalk.red(ERRORS.APP_NAME_MISSING)
-    );
-  });
-
   test(`The name app missing -> ${ERRORS.APP_NAME_MISSING}`, async () => {
     const input = '';
     expect(authValidator(input)).resolves.toBe(ERRORS.APP_NAME_MISSING);
