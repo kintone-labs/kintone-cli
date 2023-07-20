053600af344fa30daa8ab45d5c9007070e764a21
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-07-20 13:17:06 +0700

    [feat]: add unit test

---
+++ __tests__/commands/deployOptions.test.ts
@@ -0,0 +1,37 @@
+import { program, CommanderStatic } from 'commander';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import {
+  createTempDir,
+  createTemplate,
+  initProject,
+  linkDirCustom,
+  removeTempDir
+} from '../test-helpers';
+import deployCommand from '../../src/commands/Deploy/deployCommand';
+
+const PROJECT_NAME = 'test-project';
+const ORIGINAL_CWD = linkDirCustom();
+const TEMP_DIR = ORIGINAL_CWD + '/deployOptionsTemp';
+const OPTIONS = ['node', 'deploy', '--app-name', PROJECT_NAME];
+
+describe('deploy command: errors', () => {
+  let mainProgram: CommanderStatic;
+  beforeAll(async () => {
+    createTempDir(TEMP_DIR);
+
+    await initProject(TEMP_DIR, PROJECT_NAME);
+    await createTemplate(TEMP_DIR, PROJECT_NAME);
+
+    mainProgram = deployCommand(program);
+    process.argv = OPTIONS;
+    await mainProgram.parseAsync(process.argv);
+  });
+
+  afterAll(() => {
+    removeTempDir(TEMP_DIR);
+  });
+
+  test(`should have appName as ${PROJECT_NAME}`, async () => {
+    expect(mainProgram.opts().appName).toBe(PROJECT_NAME);
+  });
+});
