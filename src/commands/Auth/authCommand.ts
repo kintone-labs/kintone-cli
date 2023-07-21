2774f5a10ce285a46f274a36d59ccffa5a965abf
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-07-21 08:55:40 +0700

    [fix]: convert console log

--- src/commands/Auth/authCommand.ts
+++ src/commands/Auth/authCommand.ts
@@ -27,6 +27,7 @@ const authCommand = (program: any) => {
     .action(async (cmd) => {
       const error = authValidator(cmd);
       if (error && typeof error === 'string') {
+        console.log(chalk.red(error));
         return;
       }
       let authJSON: any;
