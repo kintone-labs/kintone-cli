89269ffe24586a5cdcd239392f2e94de022e31f5
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-07-31 11:30:59 +0700

    [chore]: change test/build ST

--- __tests__/commands/Build/deployer.test.ts
+++ __tests__/commands/Build/deployer.test.ts
@@ -2,6 +2,7 @@ import { program, Command } from 'commander';
 import buildCommand from '../../../src/commands/Build/buildCommand';
 import {
   createTempDir,
+  createTemplate,
   createTemplateSpecificType,
   initProject,
   jestCommon,
@@ -15,6 +16,7 @@ const PROJECT_NAME = 'test-project';
 const ORIGINAL_CWD = linkDirCustom();
 const TEMP_DIR = ORIGINAL_CWD + '/buildOptionsTemp';
 const APP_NAME = 'test-app';
+const WEBPACK_CONTENT = 'webpack';
 const OPTIONS = ['node', 'build', '--app-name', APP_NAME];
 const { afterAll, beforeAll, describe, expect, test } = jestCommon;
 
@@ -246,6 +248,34 @@ describe('build command', () => {
       expect(result).toEqual({
         version: ''
       });
+    });
+  });
+
+  describe('webpack', () => {
+    let mainProgram: Command;
+    const CURRENT_DIR = `${TEMP_DIR}/${PROJECT_NAME}/${APP_NAME}`;
+    const WEBPACK_DIR = `${CURRENT_DIR}/webpack.config.js`;
+
+    beforeAll(async () => {
+      createTempDir(TEMP_DIR);
+
+      await initProject(TEMP_DIR, PROJECT_NAME);
+      await createTemplate(TEMP_DIR, PROJECT_NAME);
+
+      mainProgram = buildCommand(program);
+      process.argv = OPTIONS;
+
+      writeFileSync(WEBPACK_DIR, WEBPACK_CONTENT);
+      await mainProgram.parseAsync(process.argv);
+    });
+
+    afterAll(() => {
+      removeTempDir(TEMP_DIR);
+    });
+
+    test('Should be "webpack" when setting "webpack"', () => {
+      const config = readFileSync(WEBPACK_DIR);
+      expect(config).toBe(WEBPACK_CONTENT);
     });
   });
 });
