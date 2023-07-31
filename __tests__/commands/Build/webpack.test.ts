a15e263d2658d12fbfffadb6efa4431facacdad6
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-07-31 09:09:31 +0700

    [chore]: change content

--- __tests__/commands/Build/webpack.test.ts
+++ __tests__/commands/Build/webpack.test.ts
@@ -41,7 +41,7 @@ describe('build command', () => {
       removeTempDir(TEMP_DIR);
     });
 
-    test('Should be "webpack" when assign content to "webpack" in webpack.config.js', () => {
+    test('Should be "webpack" when setting "webpack"', () => {
       const config = readFileSync(WEBPACK_DIR);
       expect(config).toBe(WEBPACK_CONTENT);
     });
