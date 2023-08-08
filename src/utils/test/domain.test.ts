297a7341a05665472c9d2e15b3b94ef60fd6038b
voduchau <h002250@cybozu.onmicrosoft.com>
2023-08-08 09:46:06 +0700

    SSR-3283: update description for ultils's unit test

--- src/utils/test/domain.test.ts
+++ src/utils/test/domain.test.ts
@@ -3,13 +3,13 @@ import { isDomain } from '../string';
 
 describe('Utils', () => {
   describe('Domain', () => {
-    test('Should be "false" when setting domain name invalid', async () => {
+    test('Should be "false" when setting invalid domain name', async () => {
       const domain = '127.0.0:8080';
 
       expect(isDomain(domain)).toBe(false);
     });
 
-    test('Should be "true" when setting domain name valid', async () => {
+    test('Should be "true" when setting valid domain name', async () => {
       const domain = 'sdd-demo.cybozu.com';
 
       expect(isDomain(domain)).toBe(true);
--- src/utils/test/url.test.ts
+++ src/utils/test/url.test.ts
@@ -3,13 +3,13 @@ import { isURL } from '../string';
 
 describe('Utils', () => {
   describe('URL', () => {
-    test('Should be "false" when setting url name invalid', async () => {
+    test('Should be "false" when setting invalid url', async () => {
       const url = 'http://www.example{23}.com/and%26here.html';
 
       expect(isURL(url)).toBe(false);
     });
 
-    test('Should be "true" when setting url name valid', async () => {
+    test('Should be "true" when setting valid url', async () => {
       const url = 'https://kintone.dev/';
 
       expect(isURL(url)).toBe(true);
