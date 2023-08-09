7785071ef7c733bee17e4e8f13ab7b1553b86bb9
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-08-09 14:22:25 +0700

    Revert "[chore]: remove unit test"
    This reverts commit 84734a0631b345ee394fb8645deb692f25f4d716.

---
+++ src/commands/Dev/test/appName.test.ts
@@ -0,0 +1,130 @@
+import { describe, expect, jest, test } from '@jest/globals';
+import { program, Command } from 'commander';
+import { readFileSync, writeFileSync } from 'jsonfile';
+
+import { dataInitDevCommand, devCommandInit, dataDemo } from './uploadConfig';
+import devCommand from '../devCommand';
+import { devCommandHandle } from '../helper';
+import {
+  APP_NAME,
+  DIR_BUILD_PATH,
+  WEBPACK_CONTENT
+} from '../../../../unit_test/constant';
+import { WRITE_FILE_OPTIONS } from '../../../constant';
+
+import {
+  authCommandImplement,
+  createTemplate,
+  getRandomProjectName,
+  initProject
+} from '../../../../unit_test/helper';
+
+const initTestProject = async () => {
+  const projectName = getRandomProjectName();
+  const currentDir = `${DIR_BUILD_PATH}/${projectName}/${APP_NAME}`;
+  const webpackDir = `${currentDir}/webpack.config.js`;
+
+  await initProject(DIR_BUILD_PATH, projectName);
+  await createTemplate(DIR_BUILD_PATH, projectName);
+
+  return webpackDir;
+};
+
+describe('dev command', () => {
+  describe('App name', () => {
+    test('Should be "test-app" when assign to "test-app"', async () => {
+      const options = ['node', 'dev', '--app-name', APP_NAME];
+      const webpackDir = await initTestProject();
+
+      authCommandImplement(program, process);
+      process.argv = options;
+      const mainProgram = devCommand(program);
+
+      writeFileSync(webpackDir, WEBPACK_CONTENT);
+      await mainProgram.parseAsync(process.argv);
+
+      expect(mainProgram.opts().appName).toBe('test-app');
+    });
+
+    test('Should be "" when assign to ""', async () => {
+      const options = ['node', 'dev', '--app-name', ''];
+      const webpackDir = await initTestProject();
+
+      const mainProgram = devCommand(program);
+      process.argv = options;
+
+      writeFileSync(webpackDir, WEBPACK_CONTENT);
+      await mainProgram.parseAsync(process.argv);
+
+      expect(mainProgram.opts().appName).toBe('');
+    });
+
+    let mainProgram: Command;
+    const readLineAsync = jest.fn();
+    test('Should be "" when assign watch to false', async () => {
+      const currentDir = await devCommandInit('Plugin');
+      const config = readFileSync(`${currentDir}/config.json`);
+
+      config.uploadConfig.desktop.css = dataDemo();
+      config.uploadConfig.mobile.js = dataDemo();
+      Object.assign(config.uploadConfig, {
+        config: {
+          css: dataDemo(),
+          js: dataDemo()
+        }
+      }).desktop.css = dataDemo();
+
+      writeFileSync(`${currentDir}/config.json`, config, WRITE_FILE_OPTIONS);
+      const { webpackDevServer, commandConfig, responseMessage } =
+        dataInitDevCommand({
+          process,
+          watch: false
+        });
+
+      try {
+        await devCommandHandle({
+          ws: webpackDevServer,
+          cmd: commandConfig,
+          data: responseMessage,
+          readLineAsync
+        });
+        expect(mainProgram.opts().appName).toBe('');
+      } catch (error) {
+        expect(error).toBe(error);
+      }
+    });
+
+    test('Should be "" when assign watch to true', async () => {
+      const currentDir = await devCommandInit('Plugin');
+      const config = readFileSync(`${currentDir}/config.json`);
+
+      config.uploadConfig.desktop.css = dataDemo();
+      config.uploadConfig.mobile.js = dataDemo();
+      Object.assign(config.uploadConfig, {
+        config: {
+          css: dataDemo(),
+          js: dataDemo()
+        }
+      }).desktop.css = dataDemo();
+
+      writeFileSync(`${currentDir}/config.json`, config, WRITE_FILE_OPTIONS);
+      const { webpackDevServer, commandConfig, responseMessage } =
+        dataInitDevCommand({
+          process,
+          watch: true
+        });
+
+      try {
+        await devCommandHandle({
+          ws: webpackDevServer,
+          cmd: commandConfig,
+          data: responseMessage,
+          readLineAsync
+        });
+        expect(mainProgram.opts().appName).toBe('');
+      } catch (error) {
+        expect(error).toBe(error);
+      }
+    });
+  });
+});
