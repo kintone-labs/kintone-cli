ad857fad82dbc01940efc6a4fcd424a1f912d1cb
Nguyen Hoang Anh <nmanhit@gmail.com>
2023-08-15 09:31:06 +0700

    set coverage for each directory

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,42 @@ const config = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   coverageThreshold: {
     global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    },
+    './src/commands/**/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Build/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Deploy/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Dev/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Initialize/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Lint/': {
       statements: 85,
       branches: 85,
       functions: 85,
