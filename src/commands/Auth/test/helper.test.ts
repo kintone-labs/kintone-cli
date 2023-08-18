b50cebcabd347679338b5b1be91d2eb83a5465fc
voduchau <h002250@cybozu.onmicrosoft.com>
2023-08-18 11:31:43 +0700

    SSR-3283: refactor for auth command

--- src/commands/Auth/test/helper.test.ts
+++ src/commands/Auth/test/helper.test.ts
@@ -1,54 +1,62 @@
 import { describe, expect, test } from '@jest/globals';
 import { authJSONAddProps, configJSONAddProps } from '../helper';
 
-describe('auth command: helper', () => {
-  test(`Should have authJSONAddProps as cmd`, () => {
-    const authJSON = {};
-    const cmd = {
-      domain: 'https://domain.kintone.com',
-      username: 'user',
-      password: 'password',
-      proxy: 'http://localhost:8080'
-    };
+describe('Auth command', () => {
+  describe('Helper', () => {
+    describe('authJSONAddProps', () => {
+      test(`Should be equivalent to cmd`, () => {
+        const authJSON = {};
+        const cmd = {
+          domain: 'https://domain.kintone.com',
+          username: 'user',
+          password: 'password',
+          proxy: 'http://localhost:8080'
+        };
 
-    const authJSONResult = cmd;
-    expect(authJSONAddProps({ authJSON, cmd })).toEqual(authJSONResult);
-  });
+        const authJSONResult = cmd;
+        expect(authJSONAddProps({ authJSON, cmd })).toEqual(authJSONResult);
+      });
 
-  test(`Should have authJSONAddProps as answer`, () => {
-    const authJSON = {};
-    const answer = {
-      domain: 'https://domain.kintone.com',
-      username: 'user',
-      password: 'password',
-      proxy: 'http://localhost:8080'
-    };
+      test(`Should be equivalent to answer`, () => {
+        const authJSON = {};
+        const answer = {
+          domain: 'https://domain.kintone.com',
+          username: 'user',
+          password: 'password',
+          proxy: 'http://localhost:8080'
+        };
 
-    const authJSONResult = answer;
-    expect(authJSONAddProps({ authJSON, answer: answer })).toEqual(
-      authJSONResult
-    );
-  });
+        const authJSONResult = answer;
+        expect(authJSONAddProps({ authJSON, answer: answer })).toEqual(
+          authJSONResult
+        );
+      });
+    });
 
-  test(`Should have configJSONAddProps as answer`, () => {
-    const configJSON = {};
-    const answer = {
-      appID: '503'
-    };
+    describe('configJSONAddProps', () => {
+      test(`Should be equivalent to answer`, () => {
+        const configJSON = {};
+        const answer = {
+          appID: '503'
+        };
 
-    const configJSONResult = answer;
-    expect(configJSONAddProps({ configJSON, answer: answer })).toEqual(
-      configJSONResult
-    );
-  });
+        const configJSONResult = answer;
+        expect(configJSONAddProps({ configJSON, answer: answer })).toEqual(
+          configJSONResult
+        );
+      });
 
-  test(`Should have configJSONAddProps as cmd`, () => {
-    const configJSON = {};
-    const cmd = {
-      appID: '503'
-    };
+      test(`Should be equivalent to cmd`, () => {
+        const configJSON = {};
+        const cmd = {
+          appID: '503'
+        };
 
-    const configJSONResult = cmd;
-    expect(configJSONAddProps({ configJSON, cmd })).toEqual(configJSONResult);
+        const configJSONResult = cmd;
+        expect(configJSONAddProps({ configJSON, cmd })).toEqual(
+          configJSONResult
+        );
+      });
+    });
   });
 });
