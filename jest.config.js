af75fe3b65bc118303f4c9b462d1522d6a414879
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-08-21 17:22:37 +0700

    [chore]: appName jest

--- jest.config.js
+++ jest.config.js
@@ -5,48 +5,12 @@ const config = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   roots: ['<rootDir>/src/'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/appName.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   coverageReporters: ['lcov'],
   verbose: true,
   coverageThreshold: {
-    global: {
-      branches: 85,
-      functions: 85,
-      lines: 85,
-      statements: 85
-    },
     './src/commands/Auth/': {
-      statements: 85,
-      branches: 85,
-      functions: 85,
-      lines: 85
-    },
-    './src/commands/Build/': {
-      statements: 85,
-      branches: 85,
-      functions: 85,
-      lines: 85
-    },
-    './src/commands/Deploy/': {
-      statements: 85,
-      branches: 85,
-      functions: 85,
-      lines: 85
-    },
-    './src/commands/Dev/': {
-      statements: 75,
-      branches: 85,
-      functions: 75,
-      lines: 75
-    },
-    './src/commands/Initialize/': {
-      statements: 85,
-      branches: 85,
-      functions: 85,
-      lines: 85
-    },
-    './src/commands/Lint/': {
       statements: 85,
       branches: 85,
       functions: 85,
