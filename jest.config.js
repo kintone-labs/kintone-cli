14d98c37fa203bb70be88386212a73a2ad33ccd9
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-08-21 14:00:38 +0700

    [chore]: add --passWithNoTests

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,43 @@ const config = {
   testMatch: ['**/test/*.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    },
     './src/commands/Auth': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Build': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Deploy': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Dev': {
+      statements: 75,
+      branches: 85,
+      functions: 75,
+      lines: 75
+    },
+    './src/commands/Initialize': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Lint': {
       statements: 85,
       branches: 85,
       functions: 85,
