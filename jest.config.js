f9c300a09ead1789d7dff947574d1ea58579b1aa
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-08-22 11:02:42 +0700

    [chore]: remove collectCoverage

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
 /** @type {import('jest').Config} */
 const config = {
   clearMocks: true,
-  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   roots: ['<rootDir>/src/'],
   testMatch: ['**/*.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  coverageReporters: ['lcov'],
-  verbose: true,
   coverageThreshold: {
     global: {
       branches: 85,
