2736d7f7b987713c852b3638bf19ef91dff6ed60
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-08-22 08:05:08 +0700

    [chore]: undo jest

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,48 @@ const config = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   roots: ['<rootDir>/src/'],
-  testMatch: ['**/appName.test.ts'],
+  testMatch: ['**/*.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   coverageReporters: ['lcov'],
   verbose: true,
   coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    },
     './src/commands/Auth/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Build/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Deploy/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Dev/': {
+      statements: 75,
+      branches: 85,
+      functions: 75,
+      lines: 75
+    },
+    './src/commands/Initialize/': {
+      statements: 85,
+      branches: 85,
+      functions: 85,
+      lines: 85
+    },
+    './src/commands/Lint/': {
       statements: 85,
       branches: 85,
       functions: 85,
