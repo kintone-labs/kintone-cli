33223e6b6339598425788a023ea823bae8cb779b
Nguyen Hoang Anh <nmanhit@gmail.com>
2023-08-22 16:55:33 +0700

    config collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
   clearMocks: true,
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   roots: ['<rootDir>/src/'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/**/*.test.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageThreshold: {
     global: {
       branches: 85,
