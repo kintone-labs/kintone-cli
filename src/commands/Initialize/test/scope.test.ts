e1ca101f7e0bb02253afdbbb8d73c55ec4db65bb
hoangtran2506 <117815925+hoangtran2506@users.noreply.github.com>
2023-08-25 09:42:39 +0700

    Revert "revert some point for check"
    This reverts commit 731c0c6395cf27642c20c976fa288a42b08b9a07.

--- src/commands/Initialize/test/scope.test.ts
+++ src/commands/Initialize/test/scope.test.ts
@@ -2,24 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import validator from '../validator';
 
 describe('Initialize command', () => {
-  describe('Validator', () => {
-    test('Should be "Invalid App Type" when setting invalid app type', async () => {
-      const params = { type: 'invalid type' };
-      const errorMessage = validator.appValidator(params) as string;
-
-      expect(errorMessage).toEqual('Invalid App Type');
-    });
-
-    test('Should be "Invalid Preset" when setting invalid preset', async () => {
-      const params = {
-        type: 'Customization',
-        preset: 'invalid preset'
-      };
-      const errorMessage = validator.appValidator(params) as string;
-
-      expect(errorMessage).toEqual('Invalid Preset');
-    });
-
+  describe('Scope', () => {
     test('Should be "Invalid Scope" when setting invalid scope', async () => {
       const params = {
         type: 'Customization',
